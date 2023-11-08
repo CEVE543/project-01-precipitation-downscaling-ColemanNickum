6ca49bd071e64ec4f0ed6461ddee43387a078cbc
ColemanNickum <cn33@rice.edu>
2023-11-08 11:00:19 -0600

    Update get_data_processing 2.jl

--- get_data_processing 2.jl
+++ get_data_processing 2.jl
@@ -2,71 +2,27 @@ using CDSAPI
 using NCDatasets
 using StatsBase: shuffle
 
-# find the "root" directory of your project
+# Find the "root" directory of your project
 HOMEDIR = abspath(dirname(@__FILE__))
 
-
-function download_and_open_data(
-  year::Int,
-  filename::AbstractString,
-  variable::AbstractString,
-  resolution=1.0,
-  bbox=[50, -130, 24, -65],
-)
-  # Download pressure level data
-  download_pressure_level_data(
-    year,
-    joinpath(filename, "geopotential_500hPa_2020.nc"),
-    "geopotential",
-    500
-  )
-
-  # Download single level data
-  download_single_level_data(
-    year,
-    joinpath(filename, "temperature_2020.nc"),
-    "2m_temperature"
-  )
-
-  # Get all files in the data directory
-  fnames = glob("*.nc", joinpath(filename))
-
-  # Open and concatenate the files
-  data_dict = open_mfdataset(fnames, variable_name)
-
-  return data_dict
-end
-
-function glob(pattern::AbstractString, dir::AbstractString=".")
-  # Convert the glob-like pattern to a regex pattern
-  regex_pattern = replace(pattern, "*" => ".*")
-  regex_pattern = Regex(regex_pattern)
-
-  # List all files in the directory and filter by the regex pattern
-  matching_files = filter(
-    filename -> occursin(regex_pattern, filename), readdir(dir; join=true)
-  )
-
-  return matching_files
+function download_2m_temperature_data(start_year, end_year, output_dir)
+    for year in start_year:end_year
+        filename = joinpath(output_dir, "2m_temperature_$year.nc")
+        variable = "2m_temperature"
+        download_single_level_data(year, filename, variable)
+    end
 end
 
 function run_demo()
+    # Define the years for which you want to download data
+    start_year = 2006
+    end_year = 2022
 
-  # The path to the raw data folder
-  data_dir = joinpath(HOMEDIR, "data")
+    # Define the output directory where the data will be saved
+    output_dir = joinpath(HOMEDIR, "data", "raw")
 
-  years = 2019:2020 # example time range
-
-  for year in years
-
-    # Download and open all data for the year
-    data_dict = download_and_open_data(year, joinpath(data_dir, "data_$year.nc"), "2m_temperature")
-
-    display(data_dict)
-
-  end
-
-  return nothing
+    # Download 2m temperature data for the specified years
+    download_2m_temperature_data(start_year, end_year, output_dir)
 end
 
 run_demo()
