975793dee7ebddbb91894e325c131faef46233cd
ColemanNickum <cn33@rice.edu>
2023-11-08 13:09:06 -0600

    Update get_data_processing 2.jl

--- get_data_processing 2.jl
+++ get_data_processing 2.jl
@@ -19,10 +19,22 @@ function run_demo()
     end_year = 2022
 
     # Define the output directory where the data will be saved
+
     output_dir = joinpath(HOMEDIR, "data", "raw")
 
     # Download 2m temperature data for the specified years
     download_2m_temperature_data(start_year, end_year, output_dir)
 end
 
-run_demo()
+# Example code to combine all 2m_temperature datasets
+# Make sure to specify the correct directory where your datasets are located
+current_dir = pwd()
+data_dir = joinpath(current_dir, "data", "raw")
+
+# List all the 2m temperature files in the data directory
+fnames = glob("2m_temperature_*.nc", data_dir)
+
+# Open and concatenate the datasets
+t2m_combined = open_mfdataset(fnames, "2m_temperature")
+
+
